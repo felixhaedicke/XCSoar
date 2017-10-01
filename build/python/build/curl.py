169dbfecd410290c1a1132344c9de1a8044514e7
Felix Hädicke <felixhaedicke@web.de>
2017-10-01 16:47:30 +0200

    build/python/libs: CURL: add workaround for compiling with "-static"
    This library contains a "libtool" script, which is used for linking.
    This tool strips the "-static" linker flag. Specifying "curl_LDFLAGS
    =-all-static" in this case solves this problem.

--- build/python/build/curl.py
+++ build/python/build/curl.py
@@ -7,3 +7,12 @@ class CurlProject(AutotoolsProject):
             self.configure_args.append('--enable-pthreads=no')
 
         return AutotoolsProject.configure(self, toolchain)
+
+    def get_make_args(self, toolchain):
+        base_make_args = AutotoolsProject.get_make_args(self, toolchain)
+
+        ldflags = (toolchain.ldflags + ' ' + self.ldflags)
+        if '-static' in ldflags:
+            return base_make_args + ['curl_LDFLAGS=-all-static']
+        else:
+            return base_make_args
